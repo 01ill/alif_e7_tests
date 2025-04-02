093df61e9faddab291a90ac8327aefd4be9e07d5
Till Billerbeck <tillbillerbeck@protonmail.com>
2025-04-02 16:17:55 +0200

    update instructions app

--- helium_instructions/main.cpp
+++ helium_instructions/main.cpp
@@ -1,4 +1,9 @@
+#include <chrono>
+#include <cstdio>
+#include <cstdint>
+#include <ctime>
 #include "RTE_Components.h"
+#include "system_M55.h"
 #include CMSIS_device_header
 
 #include "board.h"
@@ -7,7 +12,16 @@
 
 int main (void)
 {
-
+    int32_t clock = GetSystemCoreClock();
+    std::chrono::high_resolution_clock::time_point time = std::chrono::high_resolution_clock::now();
+    int32_t ms = std::chrono::duration_cast<std::chrono::milliseconds>(time.time_since_epoch()).count();
+    while (1) {
+        clock = GetSystemCoreClock();
+        time = std::chrono::high_resolution_clock::now();
+        ms = std::chrono::duration_cast<std::chrono::milliseconds>(time.time_since_epoch()).count();
+        printf("SystemCoreClock: %d\n", clock);
+    }
+    
     int val = 1;
 
     while (1) __WFI();
@@ -20,3 +34,4 @@ int _close(int val) TRAP_RET_ZERO
 int _lseek(int val0, int val1, int val2) TRAP_RET_ZERO
 int _read(int val0, char * val1, int val2) TRAP_RET_ZERO
 int _write(int val0, char * val1, int val2) TRAP_RET_ZERO
+int _write_r(int val0, char * val1, int val2) TRAP_RET_ZERO
